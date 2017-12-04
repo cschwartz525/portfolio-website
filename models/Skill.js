c0f542ff35e3ddca054715ff5c43873df7501e09
Craig Schwartz <craigschwartz525@gmail.com>
2017-12-03 23:28:13 -0500

    Added skills and skill categories

---
+++ models/Skill.js
@@ -0,0 +1,20 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Skill Model
+ * ==========
+ */
+var Skill = new keystone.List('Skill');
+
+Skill.add({
+	name: { type: String, initial: true, required: true, index: true },
+	category: { type: Types.Relationship, ref: 'SkillCategory', many: false },
+	experience: { type: Number, initial: true, required: true, index: true },
+});
+
+/**
+ * Registration
+ */
+Skill.defaultColumns = 'name, category, experience';
+Skill.register();
--- routes/views/skills.js
+++ routes/views/skills.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function (req, res) {
 
@@ -8,6 +9,33 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'skills';
+	locals.data = {
+		categories: [],
+	};
+
+	// Load all skill categories
+	view.on('init', function (next) {
+		keystone.list('SkillCategory').model.find().sort('name').exec(function (err, categories) {
+
+			if (err || !categories.length) {
+				return next(err);
+			}
+
+			locals.data.categories = categories;
+
+			// Load the skills for each category
+			async.each(locals.data.categories, function (category, next) {
+
+				keystone.list('Skill').model.find().where('category', category.id).sort('name').exec(function (err, skills) {
+					category.skills = skills;
+					next(err);
+				});
+
+			}, function (err) {
+				next(err);
+			});
+		});
+	});
 
 	// Render the view
 	view.render('skills');
