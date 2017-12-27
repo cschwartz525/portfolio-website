a0b2a27cc3b2e30219377dbc2279174e66e22c6c
Craig Schwartz <craigschwartz525@gmail.com>
2017-12-26 22:36:28 -0500

    Load jobs and education on experience page

--- routes/views/experience.js
+++ routes/views/experience.js
@@ -1,3 +1,4 @@
+var dateformat = require('dateformat');
 var keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
@@ -8,6 +9,55 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'experience';
+
+	locals.data = {
+		jobs: [],
+		education: [],
+	};
+
+	// Load all jobs
+	view.on('init', function (next) {
+		keystone.list('Job').model.find().sort('-startDate').exec(function (err, jobs) {
+
+			if (err || !jobs.length) {
+				return next(err);
+			}
+
+			jobs.forEach((job) => {
+				job.displayStartDate = dateformat(job.startDate, 'mm/yyyy');
+				if (!job.endDate) {
+					job.displayEndDate = 'Present';
+				} else {
+					job.displayEndDate = dateformat(job.endDate, 'mm/yyyy');
+				}
+			});
+
+			locals.data.jobs = jobs;
+			next(err);
+		});
+	});
+
+	// Load all education
+	view.on('init', function (next) {
+		keystone.list('Education').model.find().sort('-startDate').exec(function (err, education) {
+
+			if (err || !education.length) {
+				return next(err);
+			}
+
+			education.forEach((edu) => {
+				edu.displayStartDate = dateformat(edu.startDate, 'mm/yyyy');
+				if (!edu.endDate) {
+					edu.displayEndDate = 'Present';
+				} else {
+					edu.displayEndDate = dateformat(edu.endDate, 'mm/yyyy');
+				}
+			});
+
+			locals.data.education = education;
+			next(err);
+		});
+	});
 
 	// Render the view
 	view.render('experience');
